2772f02a09e62a4265d9ccc71dbb916476953675
User2 odiriteddie <oadamu@bargainparrot.com>
2024-07-17 23:34:52 +0100

    more base styles

---
+++ script/index.js
@@ -0,0 +1,27 @@
+// JavaScript to open and close the modal
+
+// Get the modal
+const modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn"); // Replace with your button's ID
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};

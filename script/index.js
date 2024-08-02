a5757ff1294f86166f125ad816a75fa5435f85cb
User2 odiriteddie <oadamu@bargainparrot.com>
2024-08-03 00:41:15 +0100

    last screens for user account

--- script/index.js
+++ script/index.js
@@ -1,27 +1,12 @@
-// JavaScript to open and close the modal
+const showBtn = document.getElementById("show-dialog");
+const dialog = document.getElementById("dialog");
+const jsCloseBtn = dialog.querySelector("#close-dialog");
 
-// Get the modal
-const modal = document.getElementById("modal");
+showBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn"); // Replace with your button's ID
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+jsCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
